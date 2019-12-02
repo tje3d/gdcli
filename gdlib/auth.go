3f833422001d14762d0dffd6c31d4cacdfd7dc97
tje3d <tje3d@yahoo.com>
2019-12-02 18:01:40 +0330

    feat: Add method User

--- gdlib/auth.go
+++ gdlib/auth.go
@@ -16,3 +16,29 @@ func UserAdd(mobile string) string {
 
 	return sendRequest("user/add", options)
 }
+
+// User ...
+func User(
+	uuid string,
+	appVersion string,
+	company string,
+	verificationCode string,
+	mobile string,
+) string {
+	data := map[string]string{
+		"device_uniqid":     uuid,
+		"app_version":       appVersion,
+		"company":           company,
+		"verification_code": verificationCode,
+		"mobile":            mobile,
+	}
+
+	byteData, _ := json.Marshal(data)
+
+	options := RequestOptions{
+		method: "POST",
+		data:   string(byteData),
+	}
+
+	return sendRequest("user", options)
+}
--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"log"
-	"gdlib/gdlib"
-)
-
 func main() {
-	log.Println(gdlib.UserAdd("xxx"))
+	// log.Println(gdlib.UserAdd("xxx"))
 }
